c4e41e2e670409e7877e290b6a761506ec832563
Ryan <ryan.minoo@wongfong.com>
2023-02-17 10:28:19 +0800

    Initial SwiftUI try Out

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let occupations = ["Student","Developer","Designer","Manager"]
+    @State private var selectedOcupation = "Student"
+    @State private var tapCount = 0
+    @State private var name = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                    Text("I am the setting App, world!")
+                }
+                Section {
+                    Text("section, world! \(name)")
+                    Button("Tap Count \(tapCount)") {
+                        tapCount += 1
+                    }
+                }
+                Section {
+                    TextField("Entr Your name", text: $name)
+                    Picker("Select Your Occupation", selection: $selectedOcupation) {
+                        ForEach(occupations, id: \.self){
+                            Text($0)
+                        }
+                    }
+                }
+            }.navigationTitle("SwiftUI")
         }
-        .padding()
     }
 }
 
